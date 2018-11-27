8766aaf803665bb8feddb809120b7e0cdd5a8871
dante <fayzanwasim@gmail.com>
2018-11-28 02:18:09 +0500

    CLASS ACTIVITY TO_DO LIST FOR EDIT TYPE IN INPUT FORM AT TOP AND CLICK EDIT OF RESPECTIVE ROW

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -1,99 +1,4 @@
-//============================================
-//-----------  STEP - 1 ----------------------
-//============================================
-
-
-/*var todos = [];
-function add() {
-    var task = document.getElementById("task").value;
-    todos.push(task);
-    document.getElementById('todos').innerText = todos;
-}*/
-
-
-//============================================
-//-----------  STEP - 2 ----------------------
-//============================================
-
-
-/*var todos = [];
-function add() {
-    var task = document.getElementById('task').value;
-    todos.push(task);
-    document.getElementById('task').value = '';
-    show();
-}
-
-function show() {
-    var ul = document.createElement('ul');
-    ul.classList.add("list-group");
-    for(var i=0; i<todos.length; i++){
-        var li = document.createElement('li');
-        li.innerHTML  = '<li>' + todos[i] + '</li>';
-        li.classList.add("list-group-item");
-        ul.appendChild(li);
-    }
-    document.getElementById('todos').appendChild(ul);
-}*/
-
-
-//============================================
-//-----------  STEP - 3 ----------------------
-//============================================
-
-
-/*
 function getTodos() {
-    var todos = [];
-    var todos_str = localStorage.getItem('todo');
-    if(todos_str !== null)
-        todos = JSON.parse(todos_str);
-    return todos;
-}
-
-function add() {
-    var task = document.getElementById('task').value;
-    if(task.trim() == ''){
-        document.getElementById('message').style.display = 'block';
-        return false;
-    } else {
-        document.getElementById('message').style.display = 'none';
-    }
-    var todos = getTodos();
-    todos.push({task: task});
-    document.getElementById('task').value = '';
-    localStorage.setItem('todo',JSON.stringify(todos));
-    show();
-}
-
-
-function show() {
-    document.getElementById('todos').innerText = '';
-    var todos = getTodos();
-    var ul = document.createElement('ul');
-    ul.classList.add("list-group");
-    for(var i=0; i<todos.length; i++){
-        var li = document.createElement('li');
-        li.innerHTML  = '<li>' + todos[i].task + '</li>' +
-            '<button class="btn btn-danger">' +
-            '<i class="fas fa-trash-o"></i> ' +
-            '<span class="d-none d-sm-inline"> Delete </span> </button>';
-        li.classList.add("list-group-item");
-        ul.appendChild(li);
-    }
-    document.getElementById('todos').appendChild(ul);
-}
-show();
-*/
-
-
-
-//============================================
-//-----------  STEP - 4 ----------------------
-//============================================
-
-
-/*function getTodos() {
     var todos = [];
     var todos_str = localStorage.getItem('todo');
     if(todos_str !== null)
@@ -136,7 +41,14 @@ function show() {
         li.innerHTML  = '<li>' + todos[i].task + '</li>' +
             '<button class="btn btn-danger" id="' + i + '">' +
             '<i class="fa fa-trash-o"></i> ' +
-            '<span class="d-none d-sm-inline"> Delete </span> </button>';
+            '<span class="d-none d-sm-inline"> Delete </span> </button>' +
+            '<button class="btn btn-primary" id="' + i + '">' +
+            '<i class="fa fa-check"></i> ' +
+            '<span class="d-none d-sm-inline"> Done </span>' + ' </button>' +
+            '<button class="btn btn-secondary" id="' + i + '">' +
+            '<i class="fa fa-edit"></i> ' +
+            '<span class="d-none d-sm-inline"> Edit </span>' +
+            ' </button>';
         li.classList.add("list-group-item");
         if(todos[i].isDone)
             li.classList.add("done");
@@ -144,11 +56,43 @@ function show() {
     }
     document.getElementById('todos').appendChild(ul);
     var buttons = document.getElementsByClassName('btn-danger');
+
     for(var i=0; i<buttons.length; i++){
         buttons[i].addEventListener('click',remove);
     }
-}
+    var buttons2 = document.getElementsByClassName('btn-primary');
 
+    for(var i=0; i<buttons2.length; i++){
+        buttons2[i].addEventListener('click',checkbox);
+    }
+
+    var buttons3 = document.getElementsByClassName('btn-secondary');
+
+    for(var i=0; i<buttons3.length; i++){
+        buttons3[i].addEventListener('click',edit);
+    }
+}
+function edit() {
+    var todos=getTodos();
+    var id = this.getAttribute('id');
+    var search=this.parentNode.textContent;
+    var replaces=document.getElementById('task').value;
+    var rep={task: replaces, isDone: false}
+    todos[id]=rep;
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+//\"task\":\"dsa\",\"isDone\":false
+function checkbox(){
+    if(this.parentNode.style.textDecoration==="line-through")
+    {
+        this.parentNode.style.textDecoration="none";
+    }
+    else{
+        this.parentNode.style.textDecoration="line-through";
+    }
+
+}
 function isDone(e) {
     var todos = getTodos();
     if(todos[e.target.id].isDone) {
@@ -162,7 +106,7 @@ function isDone(e) {
     localStorage.setItem('todo',JSON.stringify(todos));
     show();
 }
-show();*/
+show();
 
 
 
